811b7a8f38fbe59180675a678b4c994b0e73c1a9
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2022-08-11 19:53:09 -0500

    refactor(lsp) null-ls: on-attach auto format

--- nvim/.config/nvim/lua/autocommands.lua
+++ nvim/.config/nvim/lua/autocommands.lua
@@ -1,48 +1,48 @@
 -- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
-  callback = function()
-    vim.cmd([[
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+	callback = function()
+		vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR>
       set nobuflisted
     ]])
-  end,
+	end,
 })
 
 -- Remove statusline and tabline when in Alpha
 vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "AlphaReady" },
-  callback = function()
-    vim.cmd([[
+	pattern = { "AlphaReady" },
+	callback = function()
+		vim.cmd([[
       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
     ]])
-  end,
+	end,
 })
 
 -- Set wrap and spell in markdown and gitcommit
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit", "markdown" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 vim.cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
 
 -- Fixes Autocomment
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd("set formatoptions-=cro")
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
 })
 
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
-  end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
 })
 
 -- Only highlight current line for active window.
@@ -77,15 +77,15 @@ augroup THE_PRIMEAGEN_MODIFIED
 augroup END
 ]])
 
--- Auto format on exit
+-- Go auto format on exit
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
-  return
+	return
 end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.py", "*.go", "*.html" },
-  callback = function()
-    vim.lsp.buf.formatting_sync(nil, 500)
-  end,
+	pattern = { "*.go" },
+	callback = function()
+		vim.lsp.buf.formatting_sync(nil, 500)
+	end,
 })
--- nvim/.config/nvim/lua/jc/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/jc/lsp/null-ls.lua
@@ -3,6 +3,8 @@ if not null_ls_status_ok then
 	return
 end
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
@@ -10,6 +12,19 @@ local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 	sources = {
 		-- prettier
 		formatting.prettier.with({
@@ -38,17 +53,17 @@ null_ls.setup({
 		-- Python
 		formatting.yapf,
 
-		diagnostics.mypy.with({
-			condition = function(utils)
-				return utils.root_has_file({
-					"setup.py",
-					"setup.cfg",
-					"Pipfile",
-					"requirements.txt",
-					"pyproject.toml",
-				})
-			end,
-		}),
+		-- diagnostics.mypy.with({
+		-- 	condition = function(utils)
+		-- 		return utils.root_has_file({
+		-- 			"setup.py",
+		-- 			"setup.cfg",
+		-- 			"Pipfile",
+		-- 			"requirements.txt",
+		-- 			"pyproject.toml",
+		-- 		})
+		-- 	end,
+		-- }),
 
 		-- Fish
 		formatting.fish_indent,
