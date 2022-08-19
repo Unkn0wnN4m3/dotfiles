8435ebfb39c03d373be1364e2aa9982fe89801a7
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2022-08-18 21:10:13 -0500

    feat(functions): split functions added
    Now there are two new functions to invoke a new split windows-terminal session.
    
    Wide: Opens a new terminal using wsl
    
    Pide: Opens a new terminal using powershell
    
    alt+shift+z: Zoon into focused window

---
+++ powershell/.config/powershell/functions/Pide.ps1
@@ -0,0 +1,3 @@
+function Pide {
+    wt --% new-tab -d . ; split-pane -H -d . ; split-pane -V -d . -s .70 ; split-pane -V -d . ; move-focus up
+}
---
+++ powershell/.config/powershell/functions/Wide.ps1
@@ -0,0 +1,3 @@
+function Wide {
+    wt --% new-tab -p "Debian" ; split-pane -H -p "Debian" ; split-pane -V -s .70 -p "Debian" ; split-pane -V -p "Debian"; move-focus up
+}
