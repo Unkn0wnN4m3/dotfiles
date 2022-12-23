4ff6731b9ff332c2804b40f4886af94ba1b6d122
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2022-12-22 18:52:13 -0600

    refactor(powershell): Junk removed from QuickNotes
    Removed all unnecessary functions that wrapped the logic that allowed the creation of the notes.

--- powershell/.config/powershell/functions/QuickNote.ps1
+++ powershell/.config/powershell/functions/QuickNote.ps1
@@ -13,25 +13,21 @@ function QuickNote {
     $my_documents_path = [Environment]::GetFolderPath("MyDocuments")
     $notes_path = "$my_documents_path\QuickNotes\"
 
-    function CheckPath {
-        if ( -not ( Test-Path -Path $notes_path ) ) {
+    if ( -not ( Test-Path -Path $notes_path ) ) {
+        try {
             New-Item -ItemType Directory -Path $notes_path -ErrorAction Stop
         }
-
-        return $true
-    }
-
-    function CreateNote {
-        $new_note = ( -join ( "$notes_path\$Title", ".txt" ) )
-
-        if ( Test-Path "$new_note" -PathType Leaf ) {
-            return Write-Host "Error: The note already exists!`n" -f Red
+        catch [Exception] {
+            Write-Host $_.Exception.Message -ForegroundColor Red
         }
-
-        Write-Output "$Title`n`n$Body" >> "$new_note"
     }
 
-    if (CheckPath) {
-        CreateNote
+    $note_name = -join ( ( ( "$Title" -split " " ) -join "_" ), ".txt" )
+
+    try {
+        New-Item -ItemType File -Path "$notes_path" -Name "$note_name" -Value "$Title`n`n$Body" -ErrorAction Stop
+    }
+    catch [Exception] {
+        Write-Host $_.Exception.Message -ForegroundColor Red
     }
 }
