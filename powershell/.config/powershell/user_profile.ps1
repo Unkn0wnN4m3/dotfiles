ec517e7afa68c64ad73842c9eb2ff7d068ba8a8e
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-01-27 13:54:42 -0600

    feat: Added colors and aliases
    Added aliases: py, nvimn, la
    
    fzf has a new theme. Also colors were added for some file extensions

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -10,14 +10,22 @@ if ( -not ( $Env:FZF_DEFAULT_OPTS ) ) {
     [Environment]::SetEnvironmentVariable(
         'FZF_DEFAULT_OPTS',
         '--height 50% --layout=reverse
---color fg:-1,bg:-1,hl:33,fg+:254,bg+:235,hl+:33
---color info:136,prompt:136,pointer:230,marker:230,spinner:136',
+--color=fg:#e0def4,bg:#2a273f,hl:#6e6a86
+--color=fg+:#908caa,bg+:#232136,hl+:#908caa
+--color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
+--color=marker:#ea9a97,spinner:#eb6f92,header:#ea9a97',
         'user')
 }
 
-$Extension = ".py", ".js", ".java", ".go", ".json", ".yml", ".toml"
-foreach ( $value in $Extension ) {
-    $PSStyle.FileInfo.Extension["$value"] = "`e[33;1m"
+[String[]]$c_extension = ".json", ".jsonc", ".yaml", ".toml", ".xml"
+[String[]]$p_extension = ".py", ".js", ".jsx", ".java", ".go", ".ts", ".tsx", ".bat", ".ps1"
+
+foreach ( $cvalue in $c_extension ) {
+    $PSStyle.FileInfo.Extension["$cvalue"] = "`e[33;1m"
+}
+
+foreach ( $pvalue in $p_extension ) {
+    $PSStyle.FileInfo.Extension["$pvalue"] = "`e[34;1m"
 }
 
 $psStyle.FileInfo.Directory = "`e[35;1m"
@@ -43,7 +51,7 @@ foreach ($PFunction in Get-ChildItem "$CUSTOMPSHOME\\functions") {
 }
 
 # Modules
-$PSGalleryModules = "PSReadLine", "posh-git", "npm-completion"
+[String[]]$PSGalleryModules = "PSReadLine", "posh-git", "npm-completion"
 
 foreach ($PModule in $PSGalleryModules) {
     Import-Module $PModule -ErrorAction SilentlyContinue
@@ -57,17 +65,20 @@ foreach ($PConfig in Get-ChildItem "$CUSTOMPSHOME\\conf") {
 }
 
 # Alias
-function SHL { Get-ChildItem -Name }
-function APGR { shutdown -s -t 0 }
-function RSRT { shutdown -r -t 0 }
-function SALR { shutdown -l }
+function __SHL { param( $path ) Get-ChildItem -Path $path -Name }
+function __NVIMN { param ( $path ) nvim --noplugin -u NONE $path }
+function __APGR { shutdown -s -t 0 }
+function __RSRT { shutdown -r -t 0 }
+function __SALR { shutdown -l }
 
-Set-Alias -Name Apagar -Value APGR
-Set-Alias -Name Reiniciar -Value RSRT
-Set-Alias -Name Salir -Value SALR
-Set-Alias -Name la -Value SHL
+Set-Alias -Name Apagar -Value __APGR
+Set-Alias -Name Reiniciar -Value __RSRT
+Set-Alias -Name Salir -Value __SALR
+Set-Alias -Name la -Value __SHL
 Set-Alias -Name which -Value Get-Command
 Set-Alias -Name grep -Value findstr
 Set-Alias -Name less -Value "$env:USERPROFILE\AppData\Local\Programs\Git\usr\bin\less.exe"
 Set-Alias -Name lg -Value "$env:USERPROFILE\go\bin\lazygit.exe"
 Set-Alias -Name n -Value nvim
+Set-Alias -Name nvimn -Value __NVIMN
+Set-Alias -Name py -Value python
