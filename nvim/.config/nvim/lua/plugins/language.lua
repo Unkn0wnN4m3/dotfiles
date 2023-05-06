d8abba19d8f1980bf1f2f79df41822aee100095a
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-05-05 19:52:20 -0600

    feat: Added markdown config

--- nvim/.config/nvim/lua/plugins/language.lua
+++ nvim/.config/nvim/lua/plugins/language.lua
@@ -13,7 +13,14 @@ return {
         ft = "markdown",
         version = "v0.*",
         build = "cd app && npm install",
-        init = function() vim.g.mkdp_filetypes = { "markdown" } end
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+            vim.g.mkdp_echo_preview_url = 1
+
+            if (vim.fn.has "win32" == 1) then
+                vim.g.mkdp_browser = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
+            end
+        end
     },
     {
         "mattn/emmet-vim",
