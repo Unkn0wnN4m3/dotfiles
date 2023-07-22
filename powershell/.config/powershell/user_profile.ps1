e0fd3f74fd0e6960940dc0a92a4e3dde5871c5f3
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-07-22 16:57:45 -0600

    fix: fzf colors were removed

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -27,10 +27,7 @@ if ( -not ( $env:BAT_THEME ) ) {
 if ( -not ( $Env:FZF_DEFAULT_OPTS ) ) {
     [Environment]::SetEnvironmentVariable(
             'FZF_DEFAULT_OPTS',
-            '--height 50% --layout=reverse
-            --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
-            --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
-            --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8',
+            '--height 50% --layout=reverse',
             'user')
 }
 
