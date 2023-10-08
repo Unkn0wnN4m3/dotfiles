9caae4bb63b641f72491b8012d305c41a300af54
unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-10-08 16:48:04 -0600

    refactor: easy lsp config

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -1,32 +1,40 @@
 return {
     {
         'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        branch = 'v3.x',
         lazy = true,
-        config = function()
-            -- Note: autocompletion settings will not take effect
-            require('lsp-zero.settings').preset({})
-        end
+        config = false,
+    },
+    {
+        'williamboman/mason.nvim',
+        version = "v1.8.*",
+        lazy = false,
+        config = true,
     },
 
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
+        version = "v0.*",
         event = 'InsertEnter',
         dependencies = {
-            { 'L3MON4D3/LuaSnip' },
+            {
+                'L3MON4D3/LuaSnip',
+                version = "v2.0.*"
+            },
+            {
+                'onsails/lspkind.nvim',
+                commit = '57610d5ab560c073c465d6faf0c19f200cb67e6e'
+            }
         },
         config = function()
-            -- Here is where you configure the autocompletion settings.
-            -- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
-            -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
-            require('lsp-zero.cmp').extend()
-
-            -- And you can configure cmp even more, if you want to.
             local cmp = require('cmp')
-            local cmp_action = require('lsp-zero.cmp').action()
 
             cmp.setup({
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert'
+                },
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
@@ -34,16 +42,10 @@ return {
                 formatting = {
                     fields = { 'abbr', 'kind', 'menu' },
                     format = require('lspkind').cmp_format({
-                        -- mode = 'symbol', -- show only symbol annotations
                         maxwidth = 50,
                         ellipsis_char = '...',
                     })
                 },
-                mapping = {
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-                }
             })
         end
     },
@@ -51,156 +53,46 @@ return {
     -- LSP
     {
         'neovim/nvim-lspconfig',
-        cmd = 'LspInfo',
+        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
         event = { 'BufReadPre', 'BufNewFile' },
         dependencies = {
-            { 'hrsh7th/cmp-nvim-lsp' },
             {
-                "onsails/lspkind.nvim",
-                commit = "57610d5ab560c073c465d6faf0c19f200cb67e6e"
-            },
-            { 'williamboman/mason-lspconfig.nvim' },
-            {
-                'williamboman/mason.nvim',
-                build = function()
-                    pcall(vim.cmd, 'MasonUpdate')
-                end,
+                'hrsh7th/cmp-nvim-lsp',
+                commit = '44b16d11215dce86f253ce0c30949813c0a90765'
             },
             {
-                'jose-elias-alvarez/null-ls.nvim',
-                branch = "0.7-compat",
+                'williamboman/mason-lspconfig.nvim',
+                version = 'V1.17.*'
             },
         },
         config = function()
-            -- This is where all the LSP shenanigans will live
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
 
-            local lsp = require('lsp-zero')
-            local util = require 'lspconfig.util'
-
-            lsp.on_attach(function(client, bufnr)
-                lsp.default_keymaps({ buffer = bufnr })
+            lsp_zero.on_attach(function(client, bufnr)
+                lsp_zero.default_keymaps({ buffer = bufnr })
 
                 if client.server_capabilities["documentSymbolProvider"] then
                     require("nvim-navic").attach(client, bufnr)
                 end
             end)
 
-            -- Python
-
-            local root_files = {
-                'pyproject.toml',
-                'setup.py',
-                'setup.cfg',
-                'requirements.txt',
-                'Pipfile',
-                'pyrightconfig.json',
-                '.git',
-                '.venv',
-                'venv'
-            }
-
-            require('lspconfig').pyright.setup({
-                single_file_support = true,
-                root_dir = util.root_pattern(unpack(root_files)),
-                settings = {
-                    python = {
-                        analysis = {
-                            typeCheckingMode = "basic",
-                            autoSearchPaths = true,
-                            useLibraryCodeForTypes = true,
-                            diagnosticMode = 'workspace',
-                        },
-                        disableOrganizeImports = true,
-                        venvPath = vim.fn.expand("$WORKON_HOME")
-                    },
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "pyright",
+                    "tsserver",
+                    "eslint",
+                    "clangd",
+                    "html",
+                    "cssls",
+                    "jsonls"
                 },
-            })
-
-            -- tsserver
-
-            require('lspconfig').tsserver.setup({
-                init_options = { hostInfo = 'neovim' },
-                -- handlers = {
-                --     ["textDocument/publishDiagnostics"] = vim.lsp.with(
-                --         vim.lsp.diagnostic.on_publish_diagnostics, {
-                --             -- Disable virtual_text
-                --             virtual_text = false
-                --         }
-                --     ),
-                -- },
-                filetypes = {
-                    'javascript',
-                    'javascriptreact',
-                    'javascript.jsx',
-                    'typescript',
-                    'typescriptreact',
-                    'typescript.tsx',
-                },
-                root_dir = util.root_pattern("package.json"),
-                single_file_support = false
-            })
-
-            local eslint_root_file = {
-                '.eslintrc',
-                '.eslintrc.js',
-                '.eslintrc.cjs',
-                '.eslintrc.yaml',
-                '.eslintrc.yml',
-                '.eslintrc.json',
-                'eslint.config.js',
-            }
-
-            require 'lspconfig'.eslint.setup({
-                root_dir = function(fname)
-                    eslint_root_file = util.insert_package_json(eslint_root_file, 'eslintConfig', fname)
-                    return util.root_pattern(unpack(eslint_root_file))(fname)
-                end,
-            })
-
-            require('lspconfig').clangd.setup({})
-            require('lspconfig').denols.setup({ root_dir = util.root_pattern("deno.json", "deno.jsonc") })
-            require('lspconfig').cssls.setup({})
-            require('lspconfig').jsonls.setup({})
-            require('lspconfig').html.setup({})
-            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-            lsp.format_mapping('gq', {
-                format_opts = {
-                    async = false,
-                    timeout_ms = 10000,
-                },
-                servers = {
-                    ["null-ls"] = {
-                        "python",
-                        "javascript",
-                        "javascriptreact",
-                        "typescriptreact",
-                        "typescript",
-                        "css",
-                        "json",
-                        "jsonc",
-                        "markdown",
-                        "html",
-                        "jinja.html",
-                        "htmldjango",
-                    },
-                    ["lua_ls"] = { "lua" },
-                    ["clangd"] = { "c", "cpp" }
-                }
-            })
-
-            lsp.setup()
-
-            local null_ls = require('null-ls')
-            local formatting = null_ls.builtins.formatting
-
-            null_ls.setup({
-                sources = {
-                    formatting.prettier,
-                    formatting.yapf,
-                    formatting.isort,
-                    formatting.djlint,
-                    formatting.fish_indent
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        require('lspconfig').lua_ls.setup(lua_opts)
+                    end,
                 }
             })
         end
