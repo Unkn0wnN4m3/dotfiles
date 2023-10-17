beeaf3c60d63520cd97ecb193a5f4f7977c64dac
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-10-16 18:19:56 -0600

    fix: lsp keymaps

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -4,6 +4,10 @@ return {
         branch = 'v3.x',
         lazy = true,
         config = false,
+        init = function()
+            vim.g.lsp_zero_extend_cmp = 0
+            vim.g.lsp_zero_extend_lspconfig = 0
+        end
     },
     {
         'williamboman/mason.nvim',
@@ -28,6 +32,9 @@ return {
             }
         },
         config = function()
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_cmp()
+
             local cmp = require('cmp')
             local cmp_action = require('lsp-zero').cmp_action()
 
@@ -77,11 +84,22 @@ return {
             lsp_zero.extend_lspconfig()
 
             lsp_zero.on_attach(function(client, bufnr)
-                lsp_zero.default_keymaps({ buffer = bufnr })
-
                 if client.server_capabilities["documentSymbolProvider"] then
                     require("nvim-navic").attach(client, bufnr)
                 end
+
+                -- lsp_zero.default_keymaps({ buffer = bufnr })
+                local opts = { buffer = bufnr, remap = false }
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
             end)
 
             require('mason-lspconfig').setup({
