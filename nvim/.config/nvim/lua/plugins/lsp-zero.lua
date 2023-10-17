a5bd8b8d11c9571a5f259bf90636c0717161e6da
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-10-17 14:50:04 -0600

    added format keymap

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -91,6 +91,8 @@ return {
                 -- lsp_zero.default_keymaps({ buffer = bufnr })
                 local opts = { buffer = bufnr, remap = false }
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set({ "n", "x" }, "gq",
+                    function() vim.lsp.buf.format({ async = false, timeout_ms = 10000 }) end, opts)
                 vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                 vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                 vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
