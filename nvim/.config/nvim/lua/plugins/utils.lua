c380de36e019eedcb256472ffbc6cbadac8c19f8
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-10-31 15:08:28 -0600

    feat: added navigation keymaps for gitsigns

--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -2,7 +2,6 @@ return {
     {
         "lewis6991/gitsigns.nvim",
         version = "v0.*",
-        config = true,
         event = "VeryLazy",
         dependencies = {
             {
@@ -10,6 +9,40 @@ return {
                 version = "v3.*",
             },
         },
+        config = function()
+            require("gitsigns").setup({
+                on_attach = function(bufnr)
+                    local gs = package.loaded.gitsigns
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map("n", "]c", function()
+                        if vim.wo.diff then
+                            return "]c"
+                        end
+                        vim.schedule(function()
+                            gs.next_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, { expr = true })
+
+                    map("n", "[c", function()
+                        if vim.wo.diff then
+                            return "[c"
+                        end
+                        vim.schedule(function()
+                            gs.prev_hunk()
+                        end)
+                        return "<Ignore>"
+                    end, { expr = true })
+                end,
+            })
+        end,
     },
     {
         "folke/trouble.nvim",
