5f715d74699f0731d98a98d3eba74f044b078319
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2023-12-12 13:14:57 -0600

    ref: new colorscheme

--- fish/.config/fish/conf.d/tokyonight_day.fish
+++ fish/.config/fish/conf.d/tokyonight_day.fish
@@ -1,19 +1,19 @@
-# Dracula Color Palette
-set -l foreground ebdbb2
-set -l selection 504945
-set -l comment 8f3f71
-set -l red fb4934
-set -l orange fe8019
-set -l yellow fabd2f
-set -l green b8bb26
-set -l purple d3869b
-set -l cyan 8ec07c
-set -l blue 83a598
+# TokyoNight Color Palette
+set -l foreground 3760bf
+set -l selection b6bfe2
+set -l comment 848cb5
+set -l red f52a65
+set -l orange b15c00
+set -l yellow 8c6c3e
+set -l green 587539
+set -l purple 7847bd
+set -l cyan 007197
+set -l pink 9854f1
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
 set -g fish_color_command $cyan
-set -g fish_color_keyword $blue
+set -g fish_color_keyword $pink
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
@@ -23,7 +23,7 @@ set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
-set -g fish_color_escape $blue
+set -g fish_color_escape $pink
 set -g fish_color_autosuggestion $comment
 
 # Completion Pager Colors
@@ -31,3 +31,4 @@ set -g fish_pager_color_progress $comment
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
+set -g fish_pager_color_selected_background --background=$selection
