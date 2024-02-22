89dd6df31a744f5674fc63f6d813b1fbfd1702e2
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-02-22 17:15:03 -0600

    feat: neorg was replaced with obsidian

--- lazyvim/.config/nvim/lua/plugins/lang.lua
+++ lazyvim/.config/nvim/lua/plugins/lang.lua
@@ -1,29 +1,22 @@
 return {
-  {
-    "nvim-neorg/neorg",
-    -- version = "v6.*",
-    build = ":Neorg sync-parsers",
-    event = "VeryLazy",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
-    config = function()
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.dirman"] = { -- Manages Neorg workspaces
-            config = {
-              workspaces = {
-                work = "~/neorg/work",
-                school = "~/neorg/school",
-                dev = "~/neorg/dev",
-                notes = "~/neorg/notes",
-              },
-              default_workspace = "notes",
-            },
-          },
-          ["core.completion"] = { config = { engine = "nvim-cmp" } },
-        },
-      })
-    end,
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "Personal",
+        path = "~/vaults/Personal",
+      },
+      {
+        name = "School",
+        path = "~/vaults/School",
+      },
+    },
+    notes_subdir = "notes",
   },
 }
