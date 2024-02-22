19836f78c086227d2940d7a6bab128d84decfe24
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-02-22 15:04:13 -0600

    fix: default theme was changed

--- lazyvim/.config/nvim/lua/plugins/theme.lua
+++ lazyvim/.config/nvim/lua/plugins/theme.lua
@@ -14,6 +14,13 @@ return {
   --   },
   -- },
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
