e9370083f3766351bc9637d521c9e018752c9ae4
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-05-25 18:39:18 -0600

    fix: lsp

--- nvim-simple/.config/nvim/lua/plugins/lsp.lua
+++ nvim-simple/.config/nvim/lua/plugins/lsp.lua
@@ -1,90 +1,91 @@
 return {
-  { 'neovim/nvim-lspconfig' },
-  { 'hrsh7th/cmp-nvim-lsp' },
-  { 'hrsh7th/nvim-cmp' },
-  { 'L3MON4D3/LuaSnip' },
-  { 'hrsh7th/cmp-buffer' },
-  { 'hrsh7th/cmp-path' },
-  { 'saadparwaiz1/cmp_luasnip' },
-  { 'rafamadriz/friendly-snippets' },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v3.x',
-    config = function()
-      local lsp_zero = require('lsp-zero')
-      lsp_zero.extend_lspconfig()
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v3.x',
+      lazy = true,
+      config = false,
+      init = function()
+        vim.g.lsp_zero_extend_cmp = 0
+        vim.g.lsp_zero_extend_lspconfig = 0
+      end,
+    },
 
-      lsp_zero.on_attach(function(client, bufnr)
-        lsp_zero.default_keymaps({ buffer = bufnr })
-      end)
+    -- Autocompletion
+    {
+      'hrsh7th/nvim-cmp',
+      event = 'InsertEnter',
+      dependencies = {
+        { 'L3MON4D3/LuaSnip' },
+      },
+      config = function()
+        local lsp_zero = require('lsp-zero')
+        lsp_zero.extend_cmp()
 
-      lsp_zero.set_sign_icons({
-        error = '✘',
-        warn = '▲',
-        hint = '⚑',
-        info = '»'
-      })
+        local cmp = require('cmp')
+        local cmp_action = lsp_zero.cmp_action()
 
-      local cmp = require('cmp')
-      local cmp_action = lsp_zero.cmp_action()
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      cmp.setup({
-        sources = {
-          { name = 'path' },
-          { name = 'nvim_lsp' },
-          { name = 'luasnip', keyword_length = 2 },
-          { name = 'buffer',  keyword_length = 3 },
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        snippet = {
-          expand = function(args)
-            require('luasnip').lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          -- scroll up and down the documentation window
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-          -- navigate between snippet placeholders
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        }),
-        formatting = lsp_zero.cmp_format({ details = true }),
-      })
-
-      require('lspconfig').ruff_lsp.setup({
-        on_attach = function(client, bufnr)
-          if client.name == 'ruff_lsp' then
-            client.server_capabilities.hoverProvider = false
-          end
-        end
-      })
-
-      require('lspconfig').pyright.setup({
-        settings = {
-          pyright = {
-            -- Using Ruff's import organizer
-            disableOrganizeImports = true,
+        cmp.setup({
+          formatting = lsp_zero.cmp_format({ details = true }),
+          mapping = cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-d>'] = cmp.mapping.scroll_docs(4),
+            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          }),
+          snippet = {
+            expand = function(args)
+              require('luasnip').lsp_expand(args.body)
+            end,
           },
-          python = {
-            analysis = {
-              -- Ignore all files for analysis to exclusively use Ruff for linting
-              ignore = { '*' },
+        })
+      end
+    },
+
+    -- LSP
+    {
+      'neovim/nvim-lspconfig',
+      cmd = 'LspInfo',
+      event = { 'BufReadPre', 'BufNewFile' },
+      dependencies = {
+        { 'hrsh7th/cmp-nvim-lsp' },
+      },
+      config = function()
+        local lsp_zero = require('lsp-zero')
+        lsp_zero.extend_lspconfig()
+
+        lsp_zero.on_attach(function(client, bufnr)
+          lsp_zero.default_keymaps({ buffer = bufnr })
+        end)
+
+        require('lspconfig').ruff_lsp.setup({
+          on_attach = function(client, bufnr)
+            if client.name == 'ruff_lsp' then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+        })
+        require('lspconfig').pyright.setup({
+          settings = {
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { '*' },
+              },
             },
           },
-        },
-      })
-
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-      require('lspconfig').clangd.setup({})
-      require('lspconfig').marksman.setup({})
-    end
+        })
+        local lua_opts = lsp_zero.nvim_lua_ls()
+        require('lspconfig').lua_ls.setup(lua_opts)
+        require('lspconfig').clangd.setup({})
+        require('lspconfig').marksman.setup({})
+      end
+    }
   }
 }
--- nvim-simple/.config/nvim/lua/plugins/ui.lua
+++ nvim-simple/.config/nvim/lua/plugins/ui.lua
@@ -155,78 +155,6 @@ return {
     end,
   },
   {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      {
-        "rcarriga/nvim-notify",
-        keys = {
-          {
-            "<leader>un",
-            function()
-              require("notify").dismiss({ silent = true, pending = true })
-            end,
-            desc = "Dismiss All Notifications",
-          },
-        },
-        opts = {
-          stages = "static",
-          timeout = 5000,
-          max_height = function()
-            return math.floor(vim.o.lines * 0.75)
-          end,
-          max_width = function()
-            return math.floor(vim.o.columns * 0.75)
-          end,
-          on_open = function(win)
-            vim.api.nvim_win_set_config(win, { zindex = 100 })
-          end,
-        },
-      },
-    },
-
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-      },
-    },
-    -- stylua: ignore
-    keys = {
-      { "<S-Enter>",   function() require("noice").redirect(vim.fn.getcmdline()) end,                 mode = "c",                 desc = "Redirect Cmdline" },
-      { "<leader>snl", function() require("noice").cmd("last") end,                                   desc = "Noice Last Message" },
-      { "<leader>snh", function() require("noice").cmd("history") end,                                desc = "Noice History" },
-      { "<leader>sna", function() require("noice").cmd("all") end,                                    desc = "Noice All" },
-      { "<leader>snd", function() require("noice").cmd("dismiss") end,                                desc = "Dismiss All" },
-      { "<leader>snt", function() require("noice").cmd("telescope") end,                              desc = "Noice Telescope" },
-      { "<c-f>",       function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,              expr = true,              desc = "Scroll Forward",  mode = { "i", "n", "s" } },
-      { "<c-b>",       function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,              expr = true,              desc = "Scroll Backward", mode = { "i", "n", "s" } },
-    },
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
@@ -264,5 +192,4 @@ return {
     },
   },
   { "nvim-tree/nvim-web-devicons", lazy = true },
-  { "MunifTanjim/nui.nvim",        lazy = true },
 }
