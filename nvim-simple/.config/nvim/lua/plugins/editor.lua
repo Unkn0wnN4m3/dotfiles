90f1183e3d9e20d88ca87940f06c6122ef8e8f83
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-08 19:59:56 -0600

    fix: telescope prompt changed to top

--- nvim-simple/.config/nvim/lua/plugins/editor.lua
+++ nvim-simple/.config/nvim/lua/plugins/editor.lua
@@ -106,35 +106,39 @@ return {
     version = false, -- telescope did only one release, so use HEAD for now
     keys = {
       -- find
-      { "<leader>ff", "<cmd>Telescope find_files<cr>",                               desc = "Find Files" },
-      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-      { "<leader>fg", "<cmd>Telescope git_files<cr>",                                desc = "Find Files (git-files)" },
-      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                                 desc = "Recent" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>",                                         desc = "Find Files" },
+      { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>",           desc = "Buffers" },
+      { "<leader>fg", "<cmd>Telescope git_files<cr>",                                          desc = "Find Files (git-files)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>",                                           desc = "Recent" },
       -- git
-      { "<leader>gc", "<cmd>Telescope git_commits<CR>",                              desc = "Commits" },
-      { "<leader>gs", "<cmd>Telescope git_status<CR>",                               desc = "Status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>",                                        desc = "Commits" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>",                                         desc = "Status" },
       -- search
-      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>",                desc = "Buffer" },
-      { "<leader>sc", "<cmd>Telescope command_history<cr>",                          desc = "Command History" },
-      { "<leader>sC", "<cmd>Telescope commands<cr>",                                 desc = "Commands" },
-      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",                      desc = "Document Diagnostics" },
-      { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                              desc = "Workspace Diagnostics" },
-      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                                desc = "Help Pages" },
-      { "<leader>sH", "<cmd>Telescope highlights<cr>",                               desc = "Search Highlight Groups" },
-      { "<leader>sj", "<cmd>Telescope jumplist<cr>",                                 desc = "Jumplist" },
-      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                                  desc = "Key Maps" },
-      { "<leader>sl", "<cmd>Telescope loclist<cr>",                                  desc = "Location List" },
-      { "<leader>sM", "<cmd>Telescope man_pages<cr>",                                desc = "Man Pages" },
-      { "<leader>sm", "<cmd>Telescope marks<cr>",                                    desc = "Jump to Mark" },
-      { "<leader>so", "<cmd>Telescope vim_options<cr>",                              desc = "Options" },
-      { "<leader>sR", "<cmd>Telescope resume<cr>",                                   desc = "Resume" },
-      { "<leader>sq", "<cmd>Telescope quickfix<cr>",                                 desc = "Quickfix List" },
+      { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find layout_strategy=vertical<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>",                                    desc = "Command History" },
+      { "<leader>sC", "<cmd>Telescope commands<cr>",                                           desc = "Commands" },
+      { "<leader>sd", "<cmd>Telescope diagnostics bufnr=0<cr>",                                desc = "Document Diagnostics" },
+      { "<leader>sD", "<cmd>Telescope diagnostics<cr>",                                        desc = "Workspace Diagnostics" },
+      { "<leader>sh", "<cmd>Telescope help_tags<cr>",                                          desc = "Help Pages" },
+      { "<leader>sH", "<cmd>Telescope highlights<cr>",                                         desc = "Search Highlight Groups" },
+      { "<leader>sj", "<cmd>Telescope jumplist<cr>",                                           desc = "Jumplist" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>",                                            desc = "Key Maps" },
+      { "<leader>sl", "<cmd>Telescope loclist<cr>",                                            desc = "Location List" },
+      { "<leader>sM", "<cmd>Telescope man_pages<cr>",                                          desc = "Man Pages" },
+      { "<leader>sm", "<cmd>Telescope marks<cr>",                                              desc = "Jump to Mark" },
+      { "<leader>so", "<cmd>Telescope vim_options<cr>",                                        desc = "Options" },
+      { "<leader>sR", "<cmd>Telescope resume<cr>",                                             desc = "Resume" },
+      { "<leader>sq", "<cmd>Telescope quickfix<cr>",                                           desc = "Quickfix List" },
     },
     opts = function()
       local actions = require("telescope.actions")
 
       return {
         defaults = {
+          layout_config = {
+            prompt_position = "top"
+          },
+          sorting_strategy = "ascending",
           prompt_prefix = " ",
           selection_caret = " ",
           -- open files in the first window that is an actual file.
