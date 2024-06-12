1d0aa0be9f14c691d22a33d1b5aaa8835c91e462
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-11 18:10:51 -0600

    fix: new theme was added

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -1,9 +1,12 @@
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
+# Powershell configuration directory
+$CUSTOMPSHOME = "$ENV:USERPROFILE\\.config\\powershell"
+
 # Prompt
 if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-    oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\star.omp.json" | Invoke-Expression
+    oh-my-posh init pwsh --config "$CUSTOMPSHOME\catppuccin_mod.omp.json" | Invoke-Expression
 }
 
 # Environment variables
@@ -17,9 +20,6 @@ if ( -not ( $Env:FZF_DEFAULT_OPTS ) ) {
             '--height 50% --layout=reverse',
             'user')
 }
-
-# Powershell configuration directory
-$CUSTOMPSHOME = "$ENV:USERPROFILE\\.config\\powershell"
 
 # Functions
 foreach ($PFunction in Get-ChildItem "$CUSTOMPSHOME\\functions") {
@@ -39,6 +39,7 @@ foreach ($PConfig in Get-ChildItem "$CUSTOMPSHOME\\conf") {
 function __SHL { param( $path ) Get-ChildItem -Path $path | Format-Wide -AutoSize }
 function __CUSTOM_BAT { param( $path ) if (Get-Command bat -ErrorAction SilentlyContinue) { bat -p $path }
 else { cat $path } }
+function __TOUCH { param($path) New-Item -ItemType File -Path $path }
 
 Set-Alias -Name ls -Value __SHL
 Set-Alias -Name la -Value Get-ChildItem
@@ -49,4 +50,6 @@ Set-Alias -Name less -Value "C:\Program Files\Git\usr\bin\less.exe"
 Set-Alias -Name n -Value nvim
 Set-Alias -Name jq -Value jq-Win64
 Set-Alias -Name pd -Value podman
+Set-Alias -Name lg -Value lazygit
 Set-Alias -Name cat -Value __CUSTOM_BAT
+Set-Alias -Name touch -Value __TOUCH
