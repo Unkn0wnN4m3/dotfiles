c5d2a43c889a7dcce187cff09025aab03f944e21
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-16 17:43:58 -0600

    feat: added diagnostic keymaps

--- nvim-simple/.config/nvim/init.lua
+++ nvim-simple/.config/nvim/init.lua
@@ -13,7 +13,7 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 require("lazy").setup("plugins", {
@@ -22,7 +22,7 @@ require("lazy").setup("plugins", {
     title = "Lazy",
   },
   install = {
-    colorscheme = {"catppuccin", "habamax", },
+    colorscheme = { "catppuccin", "gruvbox", "habamax", },
   },
   defaults = {
     lazy = false,
@@ -33,11 +33,11 @@ require("lazy").setup("plugins", {
 local opt = vim.opt
 
 opt.completeopt = "menu,menuone,noselect"
-opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
+opt.conceallevel = 2  -- Hide * markup for bold and italic, but not markers with substitutions
+opt.confirm = true    -- Confirm to save changes before exiting modified buffer
 opt.cursorline = true -- Enable highlighting of the current line
-opt.colorcolumn = {"80", "120"}
-opt.expandtab = true -- Use spaces instead of tabs
+opt.colorcolumn = { "80", "120" }
+opt.expandtab = true  -- Use spaces instead of tabs
 opt.fillchars = {
   foldopen = "",
   foldclose = "",
@@ -48,36 +48,36 @@ opt.fillchars = {
 }
 opt.foldlevel = 99
 opt.formatoptions = "jcroqlnt" -- tcqj
-opt.ignorecase = true -- Ignore case
-opt.inccommand = "nosplit" -- preview incremental substitute
-opt.laststatus = 3 -- global statusline
-opt.list = true -- Show some invisible characters (tabs...
-opt.mouse = "a" -- Enable mouse mode
-opt.number = true -- Print line number
-opt.pumblend = 10 -- Popup blend
-opt.pumheight = 10 -- Maximum number of entries in a popup
-opt.scrolloff = 4 -- Lines of context
+opt.ignorecase = true          -- Ignore case
+opt.inccommand = "nosplit"     -- preview incremental substitute
+opt.laststatus = 3             -- global statusline
+opt.list = true                -- Show some invisible characters (tabs...
+opt.mouse = "a"                -- Enable mouse mode
+opt.number = true              -- Print line number
+opt.pumblend = 10              -- Popup blend
+opt.pumheight = 10             -- Maximum number of entries in a popup
+opt.scrolloff = 4              -- Lines of context
 opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 2 -- Size of an indent
+opt.shiftround = true          -- Round indent
+opt.shiftwidth = 2             -- Size of an indent
 --opt.shortmess:append({ W = true, I = true, c = true, C = true })
-opt.showmode = false -- Dont show mode since we have a statusline
-opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
+opt.showmode = false           -- Dont show mode since we have a statusline
+opt.sidescrolloff = 8          -- Columns of context
+opt.signcolumn = "yes"         -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true           -- Don't ignore case with capitals
+opt.smartindent = true         -- Insert indents automatically
 opt.spelllang = { "en", "es" }
-opt.splitbelow = true -- Put new windows below current
+opt.splitbelow = true          -- Put new windows below current
 opt.splitkeep = "screen"
-opt.splitright = true -- Put new windows right of current
-opt.tabstop = 2 -- Number of spaces tabs count for
-opt.termguicolors = true -- True color support
+opt.splitright = true          -- Put new windows right of current
+opt.tabstop = 2                -- Number of spaces tabs count for
+opt.termguicolors = true       -- True color support
 opt.undofile = true
 opt.undolevels = 10000
 opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
-vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
+opt.winminwidth = 5                -- Minimum window width
+opt.wrap = false                   -- Disable line wrap
+vim.opt.path:append { '**' }       -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*', '*.pyc', '*/__pycache__/*', '.git' }
 -- Fix markdown indentation settings
 vim.g.markdown_recommended_style = 0
@@ -105,8 +105,8 @@ map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, 
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 
 -- exit insert mode
-map("i", "jk", "<ESC>", {desc = "Exit insert mode", silent = true})
-map("i", "kj", "<ESC>", {desc = "Exit insert mode", silent = true})
+map("i", "jk", "<ESC>", { desc = "Exit insert mode", silent = true })
+map("i", "kj", "<ESC>", { desc = "Exit insert mode", silent = true })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
@@ -145,6 +145,10 @@ map("n", "]b", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 map("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
+-- diagnostics
+map('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+map('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+map('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
 
 local function augroup(name)
   return vim.api.nvim_create_augroup("nvim_" .. name, { clear = true })
