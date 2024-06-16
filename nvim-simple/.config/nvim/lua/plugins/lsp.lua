aa90de2c277341582b14edf1f700cd08f11feef5
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-06-16 17:43:30 -0600

    fix!: lspzero was removed

--- nvim-simple/.config/nvim/lua/plugins/lsp.lua
+++ nvim-simple/.config/nvim/lua/plugins/lsp.lua
@@ -1,100 +1,197 @@
 return {
+  -- Based on https://lsp-zero.netlify.app/v3.x/
+
+  -- lsp
+
   {
-    {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
-      lazy = true,
-      config = false,
-      init = function()
-        vim.g.lsp_zero_extend_cmp = 0
-        vim.g.lsp_zero_extend_lspconfig = 0
-      end,
-    },
-
-    -- Autocompletion
-    {
-      'hrsh7th/nvim-cmp',
-      event = 'InsertEnter',
-      dependencies = {
-        { 'L3MON4D3/LuaSnip' },
-      },
-      config = function()
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.extend_cmp()
-
-        local cmp = require('cmp')
-        local cmp_action = lsp_zero.cmp_action()
-
-        cmp.setup({
-          preselect = 'item',
-          completion = {
-            completeopt = 'menu,menuone,noinsert'
-          },
-          formatting = lsp_zero.cmp_format({ details = true }),
-          mapping = cmp.mapping.preset.insert({
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-d>'] = cmp.mapping.scroll_docs(4),
-            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-          }),
-          snippet = {
-            expand = function(args)
-              require('luasnip').lsp_expand(args.body)
-            end,
-          },
-        })
-      end
-    },
-
-    -- LSP
-    {
-      'neovim/nvim-lspconfig',
-      cmd = 'LspInfo',
-      event = { 'BufReadPre', 'BufNewFile' },
-      dependencies = {
-        { 'hrsh7th/cmp-nvim-lsp' },
-      },
-      config = function()
-        local lsp_zero = require('lsp-zero')
-        lsp_zero.extend_lspconfig()
-
-        lsp_zero.on_attach(function(client, bufnr)
-          lsp_zero.default_keymaps({ buffer = bufnr })
-        end)
-
-        require('lspconfig').ruff_lsp.setup({
-          on_attach = function(client, bufnr)
-            if client.name == 'ruff_lsp' then
-              -- Disable hover in favor of Pyright
-              client.server_capabilities.hoverProvider = false
-            end
-          end,
-        })
-        require('lspconfig').pyright.setup({
-          settings = {
-            pyright = {
-              -- Using Ruff's import organizer
-              disableOrganizeImports = true,
-            },
-            python = {
-              analysis = {
-                -- Ignore all files for analysis to exclusively use Ruff for linting
-                ignore = { '*' },
-              },
-            },
-          },
-        })
-        local lua_opts = lsp_zero.nvim_lua_ls()
-        require('lspconfig').lua_ls.setup(lua_opts)
-        require('lspconfig').clangd.setup({})
-        require('lspconfig').marksman.setup({})
-      end
-    }
+    'williamboman/mason.nvim',
+    lazy = false,
+    config = true,
   },
   {
+    "neovim/nvim-lspconfig",
+    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP actions',
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          -- these will be buffer-local keybindings
+          -- because they only work if you have an active language server
+
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end
+      })
+
+      local _border = "rounded"
+
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = _border
+        }
+      )
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help, {
+          border = _border
+        }
+      )
+
+      vim.diagnostic.config {
+        float = { border = _border }
+      }
+
+      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      local default_setup = function(server)
+        require('lspconfig')[server].setup({
+          capabilities = lsp_capabilities,
+        })
+      end
+
+      require('mason').setup({})
+      require('mason-lspconfig').setup({
+        ensure_installed = {
+          "clangd",
+          "pyright",
+          "ruff",
+          "lua_ls",
+          "marksman"
+        },
+        handlers = {
+          default_setup,
+          lua_ls = function()
+            require('lspconfig').lua_ls.setup({
+              capabilities = lsp_capabilities,
+              settings = {
+                Lua = {
+                  runtime = {
+                    version = 'LuaJIT'
+                  },
+                  diagnostics = {
+                    globals = { 'vim' },
+                  },
+                  workspace = {
+                    library = {
+                      vim.env.VIMRUNTIME,
+                    }
+                  }
+                }
+              }
+            })
+          end,
+          pyright = function()
+            require('lspconfig').pyright.setup({
+              settings = {
+                pyright = {
+                  -- Using Ruff's import organizer
+                  disableOrganizeImports = true,
+                },
+                python = {
+                  analysis = {
+                    -- Ignore all files for analysis to exclusively use Ruff for linting
+                    -- ignore = { '*' },
+                  },
+                },
+              },
+            })
+          end,
+          ruff = function()
+            require('lspconfig').ruff.setup({
+              on_attach = function(client, _)
+                if client.name == 'ruff' then
+                  -- Disable hover in favor of Pyright
+                  client.server_capabilities.hoverProvider = false
+                end
+              end,
+            })
+          end
+        },
+      })
+    end
+  },
+
+  -- completion
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      cmp.setup({
+        preselect = 'item',
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          fields = { 'abbr', 'kind', 'menu' },
+          format = require('lspkind').cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+            ellipsis_char = '...',
+          })
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+          ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+          -- scroll up and down the documentation window
+          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+          ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+          -- Ctrl + space triggers completion menu
+          ['<C-Space>'] = cmp.mapping.complete(),
+        }),
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+      })
+    end
+  },
+
+  -- ui
+  {
     "j-hui/fidget.nvim",
-    event = "BufReadPre",
+    event = "LspAttach",
     opts = {
       notification = {
         window = {
