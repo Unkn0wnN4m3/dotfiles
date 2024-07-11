76b47460e7345d434692f6d0ce819797c65ac805
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-11 02:44:08 -0600

    feat: added borders for lsp and cmp

--- lazyvim/.config/nvim/lua/plugins/coding.lua
+++ lazyvim/.config/nvim/lua/plugins/coding.lua
@@ -1,8 +1,13 @@
+local cmp = require("cmp")
+
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "neorg" })
-    end,
+    opts = {
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+    },
   },
 }
--- lazyvim/.config/nvim/lua/plugins/ui.lua
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -119,4 +119,15 @@ return {
       },
     },
   },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        lsp_doc_border = true,
+      },
+    },
+  },
 }
