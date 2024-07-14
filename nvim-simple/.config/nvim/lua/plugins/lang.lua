44d154cd7cc41326d0fbd405a775bfc274a78f2b
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-14 01:19:08 -0600

    feat: treesitter was added

--- nvim-simple/.config/nvim/lua/plugins/lang.lua
+++ nvim-simple/.config/nvim/lua/plugins/lang.lua
@@ -18,16 +18,77 @@ return {
 		end,
 	},
 	{
-		"vim-python/python-syntax",
-		ft = "python",
-		init = function()
-			vim.cmd("let g:python_highlight_all = 1")
-		end,
-	},
-	{
 		"linux-cultist/venv-selector.nvim",
 		branch = "regexp",
 		ft = "python",
-		opts = {},
+		config = true,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		cond = function()
+			if vim.fn.has("win32") == 1 then
+				return false
+			else
+				return true
+			end
+		end,
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"c",
+					"diff",
+					"html",
+					"javascript",
+					"jsdoc",
+					"json",
+					"jsonc",
+					"lua",
+					"luadoc",
+					"luap",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"query",
+					"regex",
+					"toml",
+					"tsx",
+					"typescript",
+					"vim",
+					"vimdoc",
+					"xml",
+					"yaml",
+				},
+				textobjects = {
+					move = {
+						enable = true,
+						goto_next_start = {
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]C"] = "@class.outer",
+							["]A"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+							["[a"] = "@parameter.inner",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[C"] = "@class.outer",
+							["[A"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
 	},
 }
