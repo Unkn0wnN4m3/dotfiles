367b2fb2e63c37a88dcf169f64a5dc1d6467b58c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-14 01:29:26 -0600

    fix: notify event

--- nvim-simple/.config/nvim/lua/plugins/ui.lua
+++ nvim-simple/.config/nvim/lua/plugins/ui.lua
@@ -192,6 +192,15 @@ return {
 				lsp_doc_border = true,
 			},
 			messages = { enabled = false },
+			routes = {
+				{
+					filter = {
+						event = "notify",
+						find = "No information available",
+					},
+					opts = { skip = true },
+				},
+			},
 		},
 		dependencies = {
 			"MunifTanjim/nui.nvim",
