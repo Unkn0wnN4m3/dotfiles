ff3cd8413e86f78da748a189a1c740635e7e9df6
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-16 20:28:17 -0600

    fix: colorschemes were moved to an individual file

--- nvim-simple/.config/nvim/lua/plugins/ui.lua
+++ nvim-simple/.config/nvim/lua/plugins/ui.lua
@@ -149,37 +149,6 @@ return {
 		end,
 	},
 	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			transparent_background = true, -- disables setting the background color.
-			background = { -- :h background
-				light = "latte",
-				dark = "frappe",
-			},
-		},
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		priority = 1000,
-		opts = {
-			transparent_mode = true,
-		},
-	},
-	{
-		"EdenEast/nightfox.nvim",
-		priority = 1000,
-		opts = {
-			options = {
-				transparent = true,
-				inverse = { -- Inverse highlight for different types
-					visual = true,
-				},
-			},
-		},
-	},
-	{
 		"prichrd/netrw.nvim",
 		config = true,
 		event = "VeryLazy",
