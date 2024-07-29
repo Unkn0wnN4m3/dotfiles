ea0606b5beb3a5604764b3dcc02880967cb90051
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-07-29 15:44:14 -0600

    feat: updated plugins

--- nvim-simple/.config/nvim/lua/plugins/lang.lua
+++ nvim-simple/.config/nvim/lua/plugins/lang.lua
@@ -1,5 +1,33 @@
 return {
 	{
+		"epwalsh/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		event = {
+			-- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+			-- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+			-- "BufReadPre ~\\Proton Drive\\**.md",
+			-- "BufNewFile ~\\Proton Drive\\**.md",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "School",
+					path = "~\\Proton Drive\\main\\My files\\Vaults\\School",
+				},
+				{
+					name = "Personal",
+					path = "~\\Proton Drive\\main\\My files\\Vaults\\Personal",
+				},
+			},
+		},
+	},
+	{
 		"linux-cultist/venv-selector.nvim",
 		branch = "regexp",
 		ft = "python",
