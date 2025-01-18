9ec0ab2375ed2d3ba86a3c3aed0324af7503e97e
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-01-17 18:16:34 -0600

    lazyvim was updated

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -7,19 +7,30 @@ opt.colorcolumn = { "80", "120" }
 opt.relativenumber = false
 opt.clipboard = "unnamed"
 
+vim.g.autoformat = false
+vim.g.snacks_animate = false
+
 -- providers
-vim.cmd("let g:python3_host_prog = '~/.virtualenvs/neovim/bin/python3'")
-vim.cmd("let g:node_host_prog = '~/.binaries/neovim/node_modules/.bin/neovim-node-host'")
+-- vim.cmd("let g:python3_host_prog = '~/.virtualenvs/neovim/bin/python3'")
+-- vim.cmd("let g:node_host_prog = '~/.binaries/neovim/node_modules/.bin/neovim-node-host'")
 vim.cmd("let g:loaded_perl_provider = 0")
 vim.cmd("let g:loaded_ruby_provider = 0")
 
 -- theme
 -- set dark or light background theme
 if vim.env.NVIM_BACKGROUND == "light" then
-  vim.o.background = "light"
+  opt.background = "light"
 else
-  vim.o.background = "dark"
+  opt.background = "dark"
 end
 
 -- lazyvim
 vim.g.lazyvim_python_lsp = "pyright"
+
+vim.cmd([[
+		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+		let &shellcmdflag = '-nol -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+		let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+		set shellquote= shellxquote=
+]])
--- lazyvim/.config/nvim/lua/plugins/ui.lua
+++ lazyvim/.config/nvim/lua/plugins/ui.lua
@@ -106,15 +106,15 @@ return {
       },
     },
   },
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        lsp_doc_border = true,
-      },
-    },
-  },
+  -- {
+  --   "folke/noice.nvim",
+  --   opts = {
+  --     presets = {
+  --       bottom_search = true,
+  --       command_palette = true,
+  --       long_message_to_split = true,
+  --       lsp_doc_border = true,
+  --     },
+  --   },
+  -- },
 }
