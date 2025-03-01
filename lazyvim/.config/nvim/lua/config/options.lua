07230e9dbd8fe18004a1dadb49812edf5913348c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-01 14:46:52 -0600

    feat: neovide window

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -47,4 +47,7 @@ if vim.g.neovide then
   vim.g.neovide_remember_window_size = false
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
+  vim.g.neovide_title_background_color =
+    string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+  vim.g.neovide_title_text_color = "pink"
 end
