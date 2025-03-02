6e5e54158588ce4ac9e8fd76419e369d22916aca
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-01 18:55:55 -0600

    fix: disable background image

--- wezterm/.config/wezterm/options.lua
+++ wezterm/.config/wezterm/options.lua
@@ -5,11 +5,13 @@ local M = {}
 
 function M.apply_to_config(config)
 	-- Basic configuration
+
 	if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		config.default_prog = { "pwsh.exe", "-NoLogo" }
 	end
 
 	local appearance = helper_functions.get_appearance()
+
 	if appearance:find("Dark") then
 		config.color_scheme = "Catppuccin Macchiato"
 		config.set_environment_variables = {
@@ -28,6 +30,7 @@ function M.apply_to_config(config)
 		"Consolas",
 	})
 	config.font_size = 12
+	config.text_background_opacity = 1.0
 	config.initial_cols = 120
 	config.initial_rows = 30
 	config.default_cursor_style = "BlinkingBar"
@@ -37,10 +40,10 @@ function M.apply_to_config(config)
 	config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 	config.adjust_window_size_when_changing_font_size = false
 	config.enable_kitty_graphics = true
-	config.window_background_image = wezterm.home_dir .. "/.config/wezterm/background.png"
-	config.window_background_image_hsb = {
-		brightness = 0.03,
-	}
+	-- config.window_background_image = wezterm.home_dir .. "/.config/wezterm/background.png"
+	-- config.window_background_image_hsb = {
+	-- 	brightness = 0.1,
+	-- }
 
 	return config
 end
