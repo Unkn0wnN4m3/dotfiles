89f425a1131f2e019ba856b8566e6566bef6bc6e
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-08 16:33:40 -0600

    ref: call theme last

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,10 +7,11 @@ local theme_module = require("theme")
 
 local config = wezterm.config_builder()
 
-config = config_module.apply_to_config(config)
-config = theme_module.apply_to_config(config)
 config.launch_menu = launch_menu
 config.keys = keys
 config.mouse_bindings = mouse_bindings
+
+config = config_module.apply_to_config(config)
+config = theme_module.apply_to_config(config)
 
 return config
