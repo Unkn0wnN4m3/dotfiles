30b1c2db0ad9f96596b9d2cf8217538dd4f9ede1
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-09 16:55:43 -0600

    refactor: launch tabs were renamed

--- wezterm/.config/wezterm/launcher.lua
+++ wezterm/.config/wezterm/launcher.lua
@@ -4,30 +4,18 @@ local M = {}
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	table.insert(M, {
-		label = "Pwsh",
+		label = "New Tab (domain `local` - pwsh)",
 		args = { "pwsh.exe", "-NoLogo" },
 	})
 
 	table.insert(M, {
-		label = "PowerShell",
+		label = "New Tab (domain `local` - Powershell)",
 		args = { "powershell.exe", "-NoLogo" },
 	})
-
-	-- for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
-	-- 	local year = vsvers:gsub("Microsoft Visual Studio/", "")
-	-- 	table.insert(M, {
-	-- 		label = "x64 Native Tools VS " .. year,
-	-- 		args = {
-	-- 			"cmd.exe",
-	-- 			"/k",
-	-- 			"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
-	-- 		},
-	-- 	})
-	-- end
 end
 
 table.insert(M, {
-	label = "Nushell",
+	label = "New Tab (domain `local` - nu)",
 	args = { "nu.exe" },
 })
 
