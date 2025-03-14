e9398149423b0b3d7435e71fda1f6db254b2870a
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-13 18:37:28 -0600

    refactor: keys were updated

--- wezterm/.config/wezterm/keys.lua
+++ wezterm/.config/wezterm/keys.lua
@@ -1,94 +1,155 @@
+-- From:
+-- https://github.com/theopn/dotfiles/blob/main/wezterm/wezterm.lua
+
 local wezterm = require("wezterm")
 local act = wezterm.action
 
-return {
-	{ key = "mapped:V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-	{ key = "mapped:C", mods = "CTRL", action = wezterm.action.CopyTo("ClipboardAndPrimarySelection") },
-	{ key = "-", mods = "CTRL", action = wezterm.action.DecreaseFontSize },
-	{ key = "=", mods = "CTRL", action = wezterm.action.IncreaseFontSize },
-	{ key = "0", mods = "CTRL", action = wezterm.action.ResetFontSize },
-	{ key = "mapped:l", mods = "ALT", action = wezterm.action.ShowLauncher },
-	{
-		key = "mapped:c",
-		mods = "LEADER",
-		action = act.SpawnTab("CurrentPaneDomain"),
-	},
-	{
-		key = "mapped:&",
-		mods = "LEADER|SHIFT",
-		action = act.CloseCurrentTab({ confirm = true }),
-	},
-	{ key = "mapped:p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
-	{ key = "mapped:n", mods = "LEADER", action = act.ActivateTabRelative(1) },
-	{
-		key = "mapped:|",
-		mods = "LEADER|SHIFT",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "mapped:_",
-		mods = "LEADER|SHIFT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "mapped:x",
-		mods = "LEADER",
-		action = wezterm.action.CloseCurrentPane({ confirm = true }),
-	},
-	{
-		key = "mapped:H",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Left", 5 }),
-	},
-	{
-		key = "mapped:J",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Down", 5 }),
-	},
-	{ key = "mapped:K", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
-	{
-		key = "mapped:L",
-		mods = "LEADER",
-		action = act.AdjustPaneSize({ "Right", 5 }),
-	},
-	{
-		key = "mapped:h",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Left"),
-	},
-	{
-		key = "mapped:l",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	{
-		key = "mapped:k",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	{
-		key = "mapped:j",
-		mods = "LEADER",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	{
-		key = "mapped:z",
-		mods = "LEADER",
-		action = wezterm.action.TogglePaneZoomState,
-	},
-	{
-		key = "mapped:n",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "[",
-		mods = "LEADER",
-		action = wezterm.action.ActivateCopyMode,
-	},
-	{
-		key = "mapped:b",
-		mods = "LEADER|CTRL",
-		action = act.SendKey({ key = "b", mods = "CTRL" }),
-	},
-}
+local M = {}
+
+function M.apply_to_config(config)
+	config.keys = {
+		{ key = "mapped::", mods = "LEADER|SHIFT", action = act.ActivateCommandPalette },
+		{ key = "mapped:V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+		{ key = "mapped:C", mods = "CTRL", action = act.CopyTo("ClipboardAndPrimarySelection") },
+		{ key = "mapped:-", mods = "CTRL", action = act.DecreaseFontSize },
+		{ key = "mapped:=", mods = "CTRL", action = act.IncreaseFontSize },
+		{ key = "mapped:0", mods = "CTRL", action = act.ResetFontSize },
+		{ key = "mapped:l", mods = "ALT", action = act.ShowLauncher },
+		{
+			key = "mapped:c",
+			mods = "LEADER",
+			action = act.SpawnTab("CurrentPaneDomain"),
+		},
+		{ key = "mapped:n", mods = "LEADER", action = act.ShowTabNavigator },
+		{
+			key = "mapped:&",
+			mods = "LEADER|SHIFT",
+			action = act.CloseCurrentTab({ confirm = true }),
+		},
+		{ key = "mapped:[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+		{ key = "mapped:]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+		{
+			key = "mapped:,",
+			mods = "LEADER",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Renaming Tab Title...:" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					if line then
+						window:active_tab():set_title(line)
+					end
+				end),
+			}),
+		},
+		{
+			key = "mapped:|",
+			mods = "LEADER|SHIFT",
+			action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "mapped:_",
+			mods = "LEADER|SHIFT",
+			action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "mapped:x",
+			mods = "LEADER",
+			action = act.CloseCurrentPane({ confirm = true }),
+		},
+		{
+			key = "mapped:h",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "mapped:l",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Right"),
+		},
+		{
+			key = "mapped:k",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "mapped:j",
+			mods = "LEADER",
+			action = act.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "mapped:r",
+			mods = "LEADER",
+			action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
+		},
+		{
+			key = "mapped:z",
+			mods = "LEADER",
+			action = act.TogglePaneZoomState,
+		},
+		{
+			key = "mapped:n",
+			mods = "SHIFT|CTRL",
+			action = act.ToggleFullScreen,
+		},
+		{
+			key = "y",
+			mods = "LEADER",
+			action = act.ActivateCopyMode,
+		},
+		{
+			key = "mapped:b",
+			mods = "LEADER|CTRL",
+			action = act.SendKey({ key = "b", mods = "CTRL" }),
+		},
+		{
+			key = "mapped:C",
+			mods = "LEADER",
+			action = act.PromptInputLine({
+				description = wezterm.format({
+					{ Attribute = { Intensity = "Bold" } },
+					{ Foreground = { AnsiColor = "Fuchsia" } },
+					{ Text = "Enter name for new workspace" },
+				}),
+				action = wezterm.action_callback(function(window, pane, line)
+					-- line will be `nil` if they hit escape without entering anything
+					-- An empty string if they just hit enter
+					-- Or the actual line of text they wrote
+					if line then
+						window:perform_action(
+							act.SwitchToWorkspace({
+								name = line,
+							}),
+							pane
+						)
+					end
+				end),
+			}),
+		},
+	}
+
+	config.key_tables = {
+		resize_pane = {
+			{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+			{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+			{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+			{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+			{ key = "Escape", action = "PopKeyTable" },
+			{ key = "Enter", action = "PopKeyTable" },
+		},
+	}
+
+	for i = 1, 9 do
+		table.insert(config.keys, {
+			key = tostring(i),
+			mods = "LEADER",
+			action = act.ActivateTab(i - 1),
+		})
+	end
+
+	return config
+end
+
+return M
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,8 +11,8 @@ local tabline = require("tabline")
 local config = wezterm.config_builder()
 
 config.launch_menu = launch_menu
-config.keys = keys
 config.mouse_bindings = mouse_bindings
+config = keys.apply_to_config(config)
 
 config = theme_module.apply_to_config(config)
 config = config_module.apply_to_config(config)
