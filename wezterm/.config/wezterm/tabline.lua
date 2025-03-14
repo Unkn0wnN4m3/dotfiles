8665464785ff9b6daa49217c1e7e30ab8a3d8d98
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-13 19:09:17 -0600

    feat: leader key indicator

--- wezterm/.config/wezterm/tabline.lua
+++ wezterm/.config/wezterm/tabline.lua
@@ -3,6 +3,14 @@ local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabl
 
 local M = {}
 
+local function leader(window)
+	if window:leader_is_active() then
+		return " " .. wezterm.nerdfonts.fa_rocket
+	else
+		return ""
+	end
+end
+
 function M.apply_to_config(config)
 	config.tab_bar_at_bottom = false
 
@@ -11,12 +19,12 @@ function M.apply_to_config(config)
 			-- theme = "rose-pine-dawn",
 			theme = config.color_scheme,
 			section_separators = {
-				left = " ",
-				right = " ",
+				left = "",
+				right = "",
 			},
 			component_separators = {
-				left = " ",
-				right = " ",
+				left = "",
+				right = "",
 			},
 			tab_separators = {
 				left = "|",
@@ -24,20 +32,12 @@ function M.apply_to_config(config)
 			},
 		},
 		sections = {
-			-- tabline_a = { "mode" },
-			-- tabline_b = { "workspace" },
-			tabline_a = { "workspace" },
-			tabline_b = {},
+			tabline_a = { leader, "workspace" },
+			tabline_b = { "hostname" },
 			tabline_c = { " " },
-			tab_active = {
-				"index",
-				{ "parent", padding = 0 },
-				"/",
-				{ "cwd", padding = { left = 0, right = 1 } },
-				{ "zoomed", padding = 0 },
-			},
+			tab_active = { "index", "tab", { "zoomed", padding = 0 } },
 			tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
-			tabline_x = {},
+			tabline_x = { " " },
 			tabline_y = { "datetime" },
 			tabline_z = { "domain" },
 		},
@@ -45,6 +45,7 @@ function M.apply_to_config(config)
 
 	tabline.setup(opts)
 
+	-- https://github.com/michaelbrusegard/tabline.wez/discussions/3
 	tabline.apply_to_config(config)
 
 	return config
