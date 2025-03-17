68227782f4b681235a23c3fe76c132aa936189d9
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-16 23:54:20 -0600

    feat(editor): zoxide keybindig was added

--- lazyvim/.config/nvim/lua/plugins/editor.lua
+++ lazyvim/.config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,9 @@
 return {
   { "tiagovla/scope.nvim", config = true },
+  {
+    "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader>fz", "<cmd>FzfLua zoxide<cr>", desc = "Jump directories using zoxide" },
+    },
+  },
 }
