d3dce5cfa13eac168a065bcac4fe8c5af8bf3ac3
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-18 09:12:27 -0600

    chore(format): theme.lua was formated

--- wezterm/.config/wezterm/theme.lua
+++ wezterm/.config/wezterm/theme.lua
@@ -12,13 +12,13 @@ function M.apply_to_config(config)
 	local catppuccin = is_dark and "Catppuccin Macchiato" or "Catppuccin Latte"
 	local rose_pine = is_dark and "rose-pine-moon" or "rose-pine-dawn"
 	local solarized = is_dark and "Solarized (dark) (terminal.sexy)" or "Solarized (light) (terminal.sexy)"
-  local tokyonight = is_dark and "tokyonight_storm" or "tokyonight_day"
+	local tokyonight = is_dark and "tokyonight_storm" or "tokyonight_day"
 
 	local schemes = {
 		["solarized-osaka"] = solarized,
 		["rose-pine"] = rose_pine,
 		["catppuccin"] = catppuccin,
-    ["tokyonight"] = tokyonight,
+		["tokyonight"] = tokyonight,
 	}
 
 	config.color_scheme = schemes[color_scheme] or catppuccin
