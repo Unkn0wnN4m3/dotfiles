b149afa28de6ebeb67d63c831e9bbca718e7d9d1
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-20 21:46:56 -0600

    feat(neovide): rezise keymaps were added

--- lazyvim/.config/nvim/lua/config/options.lua
+++ lazyvim/.config/nvim/lua/config/options.lua
@@ -49,4 +49,15 @@ if vim.g.neovide then
   vim.g.neovide_title_background_color =
     string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
   vim.g.neovide_title_text_color = "#FF69B4"
+
+  -- NOTE: Why this is not difined by default?
+  vim.keymap.set({ "n", "i" }, "<C-=>", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+  end, { noremap = true, silent = true, desc = "Increase Neovide scale factor" })
+  vim.keymap.set({ "n", "i" }, "<C-->", function()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+  end, { noremap = true, silent = true, desc = "Decrease Neovide scale factor" })
+  vim.keymap.set({ "n", "i" }, "<C-0>", function()
+    vim.g.neovide_scale_factor = 1.0
+  end, { noremap = true, silent = true, desc = "Reset Neovide scale factor" })
 end
