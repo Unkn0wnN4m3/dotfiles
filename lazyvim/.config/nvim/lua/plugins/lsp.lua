1e62520af8de711281a258b05ecb4f7616f84cdc
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-03-27 15:32:33 -0600

    feat: using virtual lines over virtual text

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    opts.diagnostics.virtual_text.prefix = "󰆦"
+    opts.diagnostics.virtual_text = false
+    -- opts.diagnostics.virtual_text.prefix = "󰆦"
+    -- opts.diagnostics.virtual_text.current_line = true
+
+    opts.diagnostics.virtual_lines = { current_line = true }
   end,
 }
