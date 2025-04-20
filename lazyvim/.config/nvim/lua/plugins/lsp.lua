b34ba65b1732d85d62d45e60e4a93407fcd479b6
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-04-19 20:03:15 -0600

    fix: virtual text comes back

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,10 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    opts.diagnostics.virtual_text = false
-    -- opts.diagnostics.virtual_text.prefix = "󰆦"
-    -- opts.diagnostics.virtual_text.current_line = true
-
-    opts.diagnostics.virtual_lines = { current_line = true }
+    opts.diagnostics.virtual_text = { prefix = "󰆦" }
+    -- opts.diagnostics.virtual_lines = { current_line = true }
   end,
 }
