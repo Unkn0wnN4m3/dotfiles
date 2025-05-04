3191ecb0241560bf6d75c73ca31ca630998abb3d
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-03 20:16:25 -0600

    feat: typst language was added

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,17 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    opts.diagnostics.virtual_text = { prefix = "󰆦" }
+    -- opts.diagnostics.virtual_text = { prefix = "󰆦" }
     -- opts.diagnostics.virtual_lines = { current_line = true }
+
+    opts.servers = {
+      tinymist = {
+        settings = {
+          formatterMode = "typstyle",
+          exportPdf = "onSave",
+          semanticTokens = "disable",
+        },
+      },
+    }
   end,
 }
---
+++ lazyvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
+      "typst",
+      "nu",
+      "powershell",
+    })
+  end,
+}
