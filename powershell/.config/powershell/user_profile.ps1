6eb2be0c3f6583b4c24bf5907cbec56a8a702d24
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-04 16:10:06 -0600

    feat: aliae config added to powershell

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -1,13 +1,8 @@
 using namespace System.Management.Automation
 using namespace System.Management.Automation.Language
 
-# Powershell configuration directory
-[string]$script:CUSTOMPSHOME = "$ENV:USERPROFILE\.config\powershell"
-# $env:VIRTUAL_ENV_DISABLE_PROMPT = 1
-
-# Prompt
-if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-  oh-my-posh init pwsh --config "$CUSTOMPSHOME\catppuccin_macchiato.omp.json" | Invoke-Expression
+if (Get-Command aliae -ErrorAction SilentlyContinue) {
+  aliae init pwsh | Invoke-Expression
 }
 
 # if (Get-Command starship -ErrorAction SilentlyContinue) {
@@ -27,54 +22,3 @@ if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
 #
 #   Invoke-Expression (&starship init powershell)
 # }
-
-# Environment variables
-if ( -not ( $env:BAT_THEME ) ) {
-  [Environment]::SetEnvironmentVariable('BAT_THEME', 'base16', 'user')
-}
-
-if ( -not ( $Env:FZF_DEFAULT_OPTS ) ) {
-  [Environment]::SetEnvironmentVariable(
-      'FZF_DEFAULT_OPTS',
-      '--height 50% --layout=reverse',
-      'user')
-}
-
-# Functions
-foreach ($PFunction in Get-ChildItem "$CUSTOMPSHOME\functions") {
-  if ($PFunction.name -match "\\*.ps1") {
-    . $PFunction
-  }
-}
-
-# conf
-foreach ($PConfig in Get-ChildItem "$CUSTOMPSHOME\conf") {
-  if ($PConfig.name -match "\\*.ps1") {
-    . $PConfig
-  }
-}
-
-# Alias
-function __TOUCH { param([Parameter(ValueFromRemainingArguments=$true)]$args) Get-ChildItem -Path $args | Format-Wide -AutoSize }
-function __CUSTOM_BAT { param( $path ) if (Get-Command bat -ErrorAction SilentlyContinue) { bat -p $path } else { cat $path } }
-function __TOUCH { param([Parameter(ValueFromRemainingArguments=$true)]$args) & New-Item -ItemType File -Path $args }
-function __EXALS { param([Parameter(ValueFromRemainingArguments=$true)]$args) & eza -F --icons --group-directories-first $args }
-function __EXALA { param([Parameter(ValueFromRemainingArguments=$true)]$args) & eza -lahF --icons --group-directories-first $args }
-function __NE { param([Parameter(ValueFromRemainingArguments=$true)]$args) & neovide --size=1480x850 $args }
-
-if (Get-Command eza -ErrorAction SilentlyContinue) {
-  Set-Alias -Name ls -Value __EXALS
-  Set-Alias -Name la -Value __EXALA
-} else {
-  Set-Alias -Name ls -Value __SHL
-  Set-Alias -Name la -Value Get-ChildItem
-}
-Set-Alias -Name which -Value Get-Command
-Set-Alias -Name open -Value Invoke-Item
-Set-Alias -Name n -Value nvim
-Set-Alias -Name pd -Value podman
-Set-Alias -Name lg -Value lazygit
-Set-Alias -Name cat -Value __CUSTOM_BAT
-Set-Alias -Name touch -Value __TOUCH
-Set-Alias -Name subl -Value 'C:\Program Files\Sublime Text\subl.exe'
-Set-Alias -Name ne -Value __NE
