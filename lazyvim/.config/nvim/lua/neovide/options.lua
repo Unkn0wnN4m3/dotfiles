3631e33555ed53882b1999b7d5b4f300393c2fdc
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-05-29 15:20:53 -0600

    fix: neovide opacity setting

--- lazyvim/.config/nvim/lua/neovide/options.lua
+++ lazyvim/.config/nvim/lua/neovide/options.lua
@@ -1,6 +1,7 @@
 vim.o.guifont = "Maple Mono NF,JetBrainsMono Nerd Font Mono:h12:h12"
--- vim.g.neovide_transparency = 0.9
--- vim.g.neovide_normal_opacity = 0.95
+-- vim.g.neovide_opacity = 0.8
+-- vim.g.neovide_normal_opacity = 0.8
+vim.g.neovide_show_border = true
 vim.g.neovide_window_blurred = true
 vim.g.neovide_remember_window_size = false
 vim.g.neovide_hide_mouse_when_typing = true
