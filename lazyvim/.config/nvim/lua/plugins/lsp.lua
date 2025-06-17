a3910625bf2b7f52810f9406e8d5296a538bcc74
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-06-16 19:36:08 -0600

    feat: plugin spellwarn was added

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -13,4 +13,12 @@ return {
       },
     },
   },
+  {
+    "ravibrock/spellwarn.nvim",
+    commit = "12734b47b008d912b4925c0bc2c1248eb534409d",
+    event = "VeryLazy",
+    opts = {
+      max_file_size = 1000,
+    },
+  },
 }
