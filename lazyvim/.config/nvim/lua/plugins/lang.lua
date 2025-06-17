f79253e2e4f4c6dee707367416ff8edcd77f579d
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-06-16 19:35:26 -0600

    fix: removed obsidian.nvim

--- lazyvim/.config/nvim/lua/plugins/lang.lua
+++ lazyvim/.config/nvim/lua/plugins/lang.lua
@@ -8,44 +8,6 @@ return {
     },
   },
   {
-    "epwalsh/obsidian.nvim",
-    version = "*",
-    lazy = true,
-    ft = "markdown",
-    -- event = {
-    --   "BufReadPre " .. vim.fn.expand("~") .. "\\Proton Drive\\main\\My files\\Vaults\\School\\*.md",
-    --   "BufReadPre " .. vim.fn.expand("~") .. "\\Proton Drive\\main\\My files\\Vaults\\Personal\\*.md",
-    -- },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = vim.fn.expand("~") .. "\\Proton Drive\\main\\My files\\Vaults\\Personal",
-        },
-        {
-          name = "school",
-          path = vim.fn.expand("~") .. "\\Proton Drive\\main\\My files\\Vaults\\School",
-        },
-      },
-      mappings = {},
-      picker = {
-        name = "fzf-lua",
-        note_mappings = {
-          new = "<C-x>",
-          insert_link = "<C-l>",
-        },
-        tag_mappings = {
-          tag_note = "<C-x>",
-          insert_tag = "<C-l>",
-        },
-      },
-      ui = { enable = false },
-    },
-  },
-  {
     "razak17/tailwind-fold.nvim",
     commit = "b1e07f4734dc2bd56778ae690a3cbaeda1a53bf2",
     opts = {},
