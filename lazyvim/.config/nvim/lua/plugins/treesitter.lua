bea523c26a7560f96073d1f1f14ccf79f6eca65d
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-09-16 00:40:32 -0600

    feat: color delimiters plugin was added

--- lazyvim/.config/nvim/lua/plugins/treesitter.lua
+++ lazyvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,36 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    vim.list_extend(opts.ensure_installed, {
-      "typst",
-      "nu",
-      "powershell",
-      "css",
-      "latex",
-    })
-  end,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "typst",
+        "nu",
+        "powershell",
+        "css",
+        "latex",
+      })
+    end,
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    init = function()
+      vim.g.rainbow_delimiters = {
+        strategy = {
+          [""] = function(bufnr)
+            -- Disabled for very large files, global strategy for large files,
+            -- local strategy otherwise
+            local line_count = vim.api.nvim_buf_line_count(bufnr)
+            if line_count > 10000 then
+              return nil
+            else
+              return "rainbow-delimiters.strategy.global"
+            end
+          end,
+        },
+      }
+    end,
+  },
 }
