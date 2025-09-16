c6c3cd5e2cafc073217262ff47a4362e9ed7b8eb
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-09-16 00:40:57 -0600

    refactor: enabled completion borders

--- lazyvim/.config/nvim/lua/plugins/coding.lua
+++ lazyvim/.config/nvim/lua/plugins/coding.lua
@@ -3,6 +3,8 @@ return {
     "saghen/blink.cmp",
     opts = function(_, opts)
       opts.keymap.preset = "default"
+      opts.completion.documentation.window =
+        vim.tbl_deep_extend("force", opts.completion.documentation.window or {}, { border = "rounded" })
     end,
   },
   {
