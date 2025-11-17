e9e0637b3cfa1785a8e58c3d5764050cb93abaff
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2025-11-16 19:53:41 -0600

    refactor: use lazyextras instead of config lsps manually

--- lazyvim/.config/nvim/lua/plugins/lsp.lua
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,43 +1,5 @@
 return {
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        tinymist = {
-          settings = {
-            formatterMode = "typstyle",
-            exportPdf = "onSave",
-            semanticTokens = "disable",
-            lint = {
-              enabled = true,
-              when = "onSave",
-            },
-            preview = {
-              background = {
-                enabled = true,
-                args = { "--data-plane-host=127.0.0.1:23635", "--invert-colors=never", "--open" },
-              },
-            },
-          },
-        },
-        texlab = {
-          settings = {
-            texlab = {
-              -- forwardSearch = {
-              --   executable = "okular",
-              --   args = { "--unique", "file:%p#src:%l%f" },
-              -- },
-              chktex = {
-                onEdit = false,
-                onOpenAndSave = true,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
     "ravibrock/spellwarn.nvim",
     commit = "12734b47b008d912b4925c0bc2c1248eb534409d",
     event = "VeryLazy",
