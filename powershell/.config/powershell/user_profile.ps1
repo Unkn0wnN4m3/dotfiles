8d216c40f102f49f577ec6adea5bd66e4375b4f2
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-04 20:14:48 -0600

    fix!: startship config was moved to aliae

--- powershell/.config/powershell/user_profile.ps1
+++ powershell/.config/powershell/user_profile.ps1
@@ -4,21 +4,3 @@ using namespace System.Management.Automation.Language
 if (Get-Command aliae -ErrorAction SilentlyContinue) {
   aliae init pwsh | Invoke-Expression
 }
-
-# if (Get-Command starship -ErrorAction SilentlyContinue) {
-#   function Invoke-Starship-PreCommand {
-#     $host.ui.RawUI.WindowTitle = "$env:USERNAME@$env:COMPUTERNAME`: $pwd `a"
-#
-#     $loc = $executionContext.SessionState.Path.CurrentLocation;
-#     $prompt = "$([char]27)]9;12$([char]7)"
-#
-#     if ($loc.Provider.Name -eq "FileSystem")
-#     {
-#       $prompt += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
-#     }
-#
-#     $host.ui.Write($prompt)
-#   }
-#
-#   Invoke-Expression (&starship init powershell)
-# }
