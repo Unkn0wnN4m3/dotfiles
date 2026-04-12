170ffe93cf6a41cbf9be059a588d9915a0e3aa5b
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-12 00:47:12 -0600

    fix: spellwarn loads when spell is activated

--- basic-nvim/.config/nvim/lua/plugins/spellwarn.lua
+++ basic-nvim/.config/nvim/lua/plugins/spellwarn.lua
@@ -2,7 +2,7 @@ return {
     {
         "ravibrock/spellwarn.nvim",
         commit = "c98cfca978791cae3edaced7056bd340debee8fe",
-        ft = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+        event = "VeryLazy",
         opts = {
             max_file_size = 1000,
         },
