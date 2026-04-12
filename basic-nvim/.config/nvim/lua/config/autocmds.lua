78eefc8048d2c9accf924aaff0c3dff5e4c1b18c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-12 00:39:51 -0600

    feat!: added kickstart.nvim autocmds

--- basic-nvim/.config/nvim/lua/config/autocmds.lua
+++ basic-nvim/.config/nvim/lua/config/autocmds.lua
@@ -53,3 +53,41 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
         vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
     end,
 })
+
+-- From kickstart.nvim
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('user-lsp-attach', { clear = true }),
+    callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client:supports_method('textDocument/documentHighlight', event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('user-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('user-lsp-detach', { clear = true }),
+                callback = function(event2)
+                    vim.lsp.buf.clear_references()
+                    vim.api.nvim_clear_autocmds { group = 'user-lsp-highlight', buffer = event2.buf }
+                end,
+            })
+        end
+
+        if client and client:supports_method('textDocument/inlayHint', event.buf) then
+            vim.keymap.set("n", "<leader>uh",
+                function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end,
+                {
+                    desc = "Toggle Inlay hints"
+                })
+        end
+    end,
+})
