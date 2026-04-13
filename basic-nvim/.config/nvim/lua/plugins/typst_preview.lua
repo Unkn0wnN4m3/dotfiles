1204264f4e89b2f9e062393f12170288f054c8bb
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-12 18:20:01 -0600

    feat: add windows cmd option

--- basic-nvim/.config/nvim/lua/plugins/typst_preview.lua
+++ basic-nvim/.config/nvim/lua/plugins/typst_preview.lua
@@ -1,8 +1,17 @@
+local custom_cmd = function()
+    if vim.fn.has("win32") == 1 then
+        return "start msedge --app=%s"
+    else
+        return nil
+    end
+end
+
 return {
     'chomosuke/typst-preview.nvim',
     ft = "typst",
     version = '1.*',
     opts = {
+        open_cmd = custom_cmd(),
         dependencies_bin = {
             tinymist = "tinymist",
             websocat = nil
