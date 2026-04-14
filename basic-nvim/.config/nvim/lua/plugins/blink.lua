c18660ded095917e2557de58e3af6e1a71caab33
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-14 14:25:51 -0600

    fix: bad capabilities implementation

--- basic-nvim/.config/nvim/lua/plugins/blink.lua
+++ basic-nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,33 +2,29 @@ return {
     {
         "saghen/blink.cmp",
         event = { "InsertEnter", "CmdlineEnter" },
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        version = '1.*',
-        config = function()
-            local blink = require('blink.cmp')
-            blink.setup({
-                keymap = { preset = 'default' },
+        dependencies = { "rafamadriz/friendly-snippets" },
+        version = "1.*",
+        opts = function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+            capabilities =
+                vim.tbl_deep_extend("force", capabilities, require("blink.cmp").get_lsp_capabilities({}, false))
+            vim.lsp.config("*", {
+                capabilities = capabilities,
+            })
+
+            return {
+                keymap = { preset = "default" },
                 appearance = {
-                    nerd_font_variant = 'mono'
+                    nerd_font_variant = "mono",
                 },
                 completion = { documentation = { auto_show = false } },
                 sources = {
-                    default = { 'lsp', 'path', 'snippets', 'buffer' },
+                    default = { "lsp", "path", "snippets", "buffer" },
                 },
-                signature = { enabled = true, trigger = { enabled = true } }
-            })
-
-            vim.lsp.config('*', {
-                capabilities = blink.get_lsp_capabilities()
-            })
-
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend('force', capabilities,
-                require('blink.cmp').get_lsp_capabilities({}, false))
-            vim.lsp.config('*', {
-                capabilities = capabilities
-            })
+                signature = { enabled = true, trigger = { enabled = true } },
+            }
         end,
-        opts_extend = { "sources.default" }
-    }
+        opts_extend = { "sources.default" },
+    },
 }
