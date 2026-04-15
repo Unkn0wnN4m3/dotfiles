c6e7bd41b0b2b4377dab500ecd8b79956318cc61
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-14 19:54:12 -0600

    chore: code formated

--- basic-nvim/.config/nvim/lsp/lua_ls.lua
+++ basic-nvim/.config/nvim/lsp/lua_ls.lua
@@ -3,31 +3,35 @@ return {
         if client.workspace_folders then
             local path = client.workspace_folders[1].name
             if
-                path ~= vim.fn.stdpath('config')
-                and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                path ~= vim.fn.stdpath("config")
+                and (
+                    vim.uv.fs_stat(path .. "/.luarc.json")
+                    or vim.uv.fs_stat(path .. "/.luarc.jsonc")
+                )
             then
                 return
             end
         end
 
-        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-            runtime = {
-                version = 'LuaJIT',
-                path = {
-                    'lua/?.lua',
-                    'lua/?/init.lua',
+        client.config.settings.Lua =
+            vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                runtime = {
+                    version = "LuaJIT",
+                    path = {
+                        "lua/?.lua",
+                        "lua/?/init.lua",
+                    },
                 },
-            },
-            workspace = {
-                checkThirdParty = false,
-                library = {
-                    vim.env.VIMRUNTIME,
-                    -- Depending on the usage, you might want to add additional paths
-                    -- here.
-                    -- '${3rd}/luv/library',
-                    -- '${3rd}/busted/library',
+                workspace = {
+                    checkThirdParty = false,
+                    library = {
+                        vim.env.VIMRUNTIME,
+                        -- Depending on the usage, you might want to add additional paths
+                        -- here.
+                        -- '${3rd}/luv/library',
+                        -- '${3rd}/busted/library',
+                    },
                 },
-            },
-        })
+            })
     end,
 }
