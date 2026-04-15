0e4cac4a1a1cf4451cf3086a5008bd517704d3c7
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-14 19:54:54 -0600

    feat: added inlayhint toggle keymap

--- basic-nvim/.config/nvim/lua/config/keymaps.lua
+++ basic-nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,21 +2,52 @@ vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
 vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-vim.keymap.set("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
-vim.keymap.set("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
-vim.keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set(
+    "n",
+    "<leader><tab>o",
+    "<cmd>tabonly<cr>",
+    { desc = "Close Other Tabs" }
+)
+vim.keymap.set(
+    "n",
+    "<leader><tab>f",
+    "<cmd>tabfirst<cr>",
+    { desc = "First Tab" }
+)
+vim.keymap.set(
+    "n",
+    "<leader><tab><tab>",
+    "<cmd>tabnew<cr>",
+    { desc = "New Tab" }
+)
 vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
-vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+vim.keymap.set(
+    "n",
+    "<leader><tab>d",
+    "<cmd>tabclose<cr>",
+    { desc = "Close Tab" }
+)
+vim.keymap.set(
+    "n",
+    "<leader><tab>[",
+    "<cmd>tabprevious<cr>",
+    { desc = "Previous Tab" }
+)
 
-local lsp_keys_group = vim.api.nvim_create_augroup("user_lsp_keys", { clear = true })
+local lsp_keys_group =
+    vim.api.nvim_create_augroup("user_lsp_keys", { clear = true })
 vim.api.nvim_create_autocmd("LspAttach", {
     group = lsp_keys_group,
     callback = function(event)
         local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
         local bufnr = event.buf
 
-        if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlineCompletion, bufnr) then
+        if
+            client:supports_method(
+                vim.lsp.protocol.Methods.textDocument_inlineCompletion,
+                bufnr
+            )
+        then
             vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
 
             vim.keymap.set(
@@ -37,6 +68,25 @@ vim.api.nvim_create_autocmd("LspAttach", {
             vim.keymap.set("n", "<leader>ce", function()
                 vim.cmd("LspTinymistExportPdf")
             end, { buffer = bufnr, desc = "Export document to PDF" })
+        end
+
+        if
+            client:supports_method(
+                vim.lsp.protocol.Methods.textDocument_inlayHint,
+                bufnr
+            )
+        then
+            local inlay_hint_toggle = function()
+                vim.lsp.inlay_hint.enable(
+                    not vim.lsp.inlay_hint.is_enabled({ bufnr = bufnr }),
+                    { bufnr = bufnr }
+                )
+            end
+
+            vim.keymap.set("n", "<leader>uh", inlay_hint_toggle, {
+                desc = "Toggle inlay hints",
+                buffer = bufnr,
+            })
         end
     end,
 })
