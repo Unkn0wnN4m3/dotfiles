eece79739a2611faa61eae0c43cdb21da6f7299b
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-16 20:59:30 -0600

    fix!: removed vim figitive plugin

--- lazyvim/.config/nvim/lua/plugins/coding.lua
+++ lazyvim/.config/nvim/lua/plugins/coding.lua
@@ -7,8 +7,4 @@ return {
         vim.tbl_deep_extend("force", opts.completion.documentation.window or {}, { border = "rounded" })
     end,
   },
-  {
-    "tpope/vim-fugitive",
-    event = "VeryLazy",
-  },
 }
