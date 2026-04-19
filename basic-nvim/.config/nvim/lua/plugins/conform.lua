6fbf76891aaa8c8e90c507eb4de83324357c89d8
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-19 16:22:16 -0600

    feat: add ruff organize imports to python ft

--- basic-nvim/.config/nvim/lua/plugins/conform.lua
+++ basic-nvim/.config/nvim/lua/plugins/conform.lua
@@ -18,7 +18,7 @@ return {
         formatters_by_ft = {
             lua = { "stylua" },
             typst = { "typstyle", lsp_format = "first" },
-            python = { "ruff", lsp_format = "never" },
+            python = { "ruff", "ruff_organize_imports", lsp_format = "never" },
             markdown = { "mdformat" },
         },
         default_format_opts = {
