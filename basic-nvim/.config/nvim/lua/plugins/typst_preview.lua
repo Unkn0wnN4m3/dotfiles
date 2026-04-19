ace703578bfb675538af1cde93d53da49326eabf
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-19 16:32:22 -0600

    fix: cmd for windows

--- basic-nvim/.config/nvim/lua/plugins/typst_preview.lua
+++ basic-nvim/.config/nvim/lua/plugins/typst_preview.lua
@@ -1,21 +1,24 @@
-local custom_cmd = function()
-    if vim.fn.has("win32") == 1 then
-        return "start msedge --app=%s"
-    else
-        return nil
-    end
-end
-
 return {
     "chomosuke/typst-preview.nvim",
     version = "1.*",
-    opts = {
-        open_cmd = custom_cmd(),
-        dependencies_bin = {
-            tinymist = "tinymist",
-            websocat = nil,
-        },
-    },
+    opts = function()
+        local custom_cmd = function()
+            if vim.fn.has("win32") == 1 then
+                return "start msedge --app=%s"
+            else
+                return nil
+            end
+        end
+
+        return {
+            open_cmd = custom_cmd(),
+            dependencies_bin = {
+                tinymist = (vim.fn.has("win32") == 1) and "tinymist.cmd"
+                    or "tinymist",
+                websocat = nil,
+            },
+        }
+    end,
     keys = {
         {
             "<leader>cp",
