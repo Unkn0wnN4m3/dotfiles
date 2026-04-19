3282a529e9b71282b5d77847a384230795d84876
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-19 16:22:54 -0600

    refactor: fix lsp config

--- basic-nvim/.config/nvim/lua/plugins/lsp.lua
+++ basic-nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,39 +1,47 @@
 return {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        {
-            "mason-org/mason-lspconfig.nvim",
-            opts = { automatic_enable = { exclude = { "ruff", "stylua" } } },
-        },
-        {
-            "neovim/nvim-lspconfig",
-            config = function()
-                vim.lsp.config(
-                    "copilot",
-                    { settings = { telemetry = { telemetryLevel = "off" } } }
-                )
-                vim.lsp.config("tinymist", {
-                    settings = {
-                        formatterMode = "typstyle",
-                        formatterProseWrap = true,
-                        formatterPrintWidth = 80,
-                        formatterIndentSize = 4,
-                    },
-                })
-            end,
-        },
+    { "mason-org/mason.nvim", opts = {}, lazy = true },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        lazy = true,
+        opts = { automatic_enable = { exclude = { "ruff", "stylua" } } },
     },
-    opts = {
-        ensure_installed = {
-            "lua_ls",
-            "stylua",
-            "tinymist",
-            "copilot",
-            "basedpyright",
-            "ruff",
-            "mdformat",
+    {
+        "neovim/nvim-lspconfig",
+        lazy = true,
+        config = function()
+            vim.lsp.config("copilot", {
+                settings = {
+                    telemetry = { telemetryLevel = "off" },
+                },
+            })
+            vim.lsp.config("tinymist", {
+                settings = {
+                    formatterMode = "typstyle",
+                    formatterProseWrap = true,
+                    formatterPrintWidth = 80,
+                    formatterIndentSize = 4,
+                },
+            })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "stylua",
+                "tinymist",
+                "copilot",
+                "basedpyright",
+                "ruff",
+                "mdformat",
+            },
         },
     },
 }
