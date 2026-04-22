a2e2a78b4b2e131ea1260fe083d906cffc02499f
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-04-22 00:37:28 -0600

    fix: better theme defaults

--- basic-nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ basic-nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -38,6 +38,8 @@ return {
         "craftzdog/solarized-osaka.nvim",
         lazy = false,
         priority = 1000,
-        opts = {},
+        opts = {
+            transparent = false,
+        },
     },
 }
