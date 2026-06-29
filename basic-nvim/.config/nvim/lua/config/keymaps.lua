6c77c729c95f37b8ba37b346eec7c21c9b57f71c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-06-28 19:53:47 -0600

    refactor: using map custom function

--- basic-nvim/.config/nvim/lua/config/keymaps.lua
+++ basic-nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,76 +1,41 @@
-vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+local function map(mode, lhs, rhs, opts)
+    local options = { silent = true, noremap = true }
 
-vim.keymap.set("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
-vim.keymap.set(
-    "n",
-    "<leader><tab>o",
-    "<cmd>tabonly<cr>",
-    { desc = "Close Other Tabs" }
-)
-vim.keymap.set(
-    "n",
-    "<leader><tab>f",
-    "<cmd>tabfirst<cr>",
-    { desc = "First Tab" }
-)
-vim.keymap.set(
-    "n",
-    "<leader><tab><tab>",
-    "<cmd>tabnew<cr>",
-    { desc = "New Tab" }
-)
-vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set(
-    "n",
-    "<leader><tab>d",
-    "<cmd>tabclose<cr>",
-    { desc = "Close Tab" }
-)
-vim.keymap.set(
-    "n",
-    "<leader><tab>[",
-    "<cmd>tabprevious<cr>",
-    { desc = "Previous Tab" }
-)
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
 
-vim.keymap.set(
-    "n",
-    "<leader>bb",
-    "<cmd>e #<cr>",
-    { desc = "Switch to Other Buffer" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>bd",
-    "<cmd>bdelete<cr>",
-    { desc = "Delete Buffer" }
-)
-vim.keymap.set(
-    "n",
-    "<leader>bp",
-    "<cmd>bprevious<cr>",
-    { desc = "Prev Buffer" }
-)
-vim.keymap.set("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+    vim.keymap.set(mode, lhs, rhs, options)
+end
 
-vim.keymap.set("n", "<leader>us", function()
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
+map("n", "<leader><tab>l", "<cmd>tablast<cr>", { desc = "Last Tab" })
+map("n", "<leader><tab>o", "<cmd>tabonly<cr>", { desc = "Close Other Tabs" })
+map("n", "<leader><tab>f", "<cmd>tabfirst<cr>", { desc = "First Tab" })
+map("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New Tab" })
+map("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+map("n", "<leader><tab>d", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+map("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+map("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
+map("n", "<leader>bp", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+map("n", "<leader>us", function()
     vim.opt_local.spell = not vim.opt_local.spell:get()
 end, {
     desc = "Toggle spell check",
 })
 
-vim.keymap.set({ "i", "n", "s" }, "<esc>", function()
+map({ "i", "n", "s" }, "<esc>", function()
     vim.cmd("noh")
     return "<esc>"
 end, { expr = true, desc = "Escape and Clear hlsearch" })
 
-vim.keymap.set(
-    "n",
-    "<leader>e",
-    "<cmd>Lexplore<cr>",
-    { desc = "File Explorer" }
-)
+map("n", "<leader>e", "<cmd>Lexplore<cr>", { desc = "File Explorer" })
 
 local lsp_keys_group =
     vim.api.nvim_create_augroup("user_lsp_keys", { clear = true })
@@ -80,30 +45,30 @@ vim.api.nvim_create_autocmd("LspAttach", {
         local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
         local bufnr = event.buf
 
-        if
-            client:supports_method(
-                vim.lsp.protocol.Methods.textDocument_inlineCompletion,
-                bufnr
-            )
-        then
-            vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
-
-            vim.keymap.set(
-                "i",
-                "<C-F>",
-                vim.lsp.inline_completion.get,
-                { desc = "LSP: accept inline completion", buffer = bufnr }
-            )
-            vim.keymap.set(
-                "i",
-                "<C-G>",
-                vim.lsp.inline_completion.select,
-                { desc = "LSP: switch inline completion", buffer = bufnr }
-            )
-        end
+        -- if
+        --     client:supports_method(
+        --         vim.lsp.protocol.Methods.textDocument_inlineCompletion,
+        --         bufnr
+        --     )
+        -- then
+        --     vim.lsp.inline_completion.enable(true, { bufnr = bufnr })
+        --
+        --     map(
+        --         "i",
+        --         "<C-F>",
+        --         vim.lsp.inline_completion.get,
+        --         { desc = "LSP: accept inline completion", buffer = bufnr }
+        --     )
+        --     map(
+        --         "i",
+        --         "<C-G>",
+        --         vim.lsp.inline_completion.select,
+        --         { desc = "LSP: switch inline completion", buffer = bufnr }
+        --     )
+        -- end
 
         if client.name == "tinymist" then
-            vim.keymap.set("n", "<leader>ce", function()
+            map("n", "<leader>ce", function()
                 vim.cmd("LspTinymistExportPdf")
             end, { buffer = bufnr, desc = "Export document to PDF" })
         end
@@ -121,7 +86,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
                 )
             end
 
-            vim.keymap.set("n", "<leader>uh", inlay_hint_toggle, {
+            map("n", "<leader>uh", inlay_hint_toggle, {
                 desc = "Toggle inlay hints",
                 buffer = bufnr,
             })
